6b7cd9968d6db7b2e6b81a9a2bd3b97d54a47957
Jake Campa and Sarah Leahy <sarah@>
2017-02-22 10:10:54 -0800

    add cuisine save function

---
+++ src/Cuisine.php
@@ -0,0 +1,44 @@
+<?php
+    class Cuisine
+    {
+        private $cusine;
+        private $id;
+
+        function __construct($cuisine, $id = null)
+        {
+            $this->cuisine = $cuisine;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getCuisine()
+        {
+            return $this->cuisine;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO cuisine_type(cuisine_type) VALUES ('{$this->getCuisine()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+        static function getAll()
+        {
+            $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisine_type;");
+            var_dump($returned_cuisines);
+            $cuisines = array();
+            foreach ($returned_cuisines as $cuisine) {
+                $type = $cuisine['cuisine_type'];
+                $id = $cuisine['id'];
+                $new_cuisine = new Cuisine($type, $id);
+                array_push($cuisines, $new_cuisine);
+            }
+            return $cuisines;
+        }
+
+
+    }
+?>
---
+++ tests/CuisineTest.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Cuisine.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=cuisine_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CuisineTest extends PHPUnit_Framework_TestCase
+    {
+        function test_save()
+        {
+            $cuisine = "indian";
+            $id = null;
+            $cuisine_test = new Cuisine($cuisine, $id);
+            $cuisine_test->save();
+
+            $result = Cuisine::getAll();
+
+            $this->assertEquals(1, is_numeric($result[1]->getId()));
+
+        }
+      }
+?>
